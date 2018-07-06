078f6a2a1cf5d4df437631697078b0ebcc5d2a39
ettomato <40891361+ettomato@users.noreply.github.com>
2018-07-06 21:34:06 +0900

    Update main.js

--- main.js
+++ main.js
@@ -2,11 +2,11 @@
   'use strict';
 
   var INTERVAL = 2000;
-  var images=['slides/image1.jpg',
-              'slides/image2.jpg',
-              'slides/image3.jpg',
-              'slides/image4.jpg',
-              'slides/image5.jpg'];
+  var images=['image1.jpg',
+              'image2.jpg',
+              'image3.jpg',
+              'image4.jpg',
+              'image5.jpg'];
 
   var current=0;
   var prev = document.getElementById('prev');
